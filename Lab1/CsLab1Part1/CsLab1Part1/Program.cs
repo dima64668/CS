898168a465c3d87cc304b0b5769a256acc4ec723
Dmytro Momot <dima64668@gmail.com>
2020-02-19 18:43:31 +0200

    push

---
+++ Lab1/CsLab1Part1/CsLab1Part1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsLab1Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Analysis(Directory.GetCurrentDirectory() + @"\texts\t1.txt");
+            Console.WriteLine();
+            Console.WriteLine();
+            Analysis(Directory.GetCurrentDirectory() + @"\texts\t2.txt");
+            Console.WriteLine();
+            Console.WriteLine();
+            Analysis(Directory.GetCurrentDirectory() + @"\texts\t3.txt");
+            Console.ReadKey();
+            //
+            //Analysis(Directory.GetCurrentDirectory() + @"\texts\t1_base64.txt");
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Analysis(Directory.GetCurrentDirectory() + @"\texts\t2_base64.txt");
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Analysis(Directory.GetCurrentDirectory() + @"\texts\t3_base64.txt");
+            //Console.ReadKey();
+            //
+            //Analysis(Directory.GetCurrentDirectory() + @"\texts\t1_base64.bz2");
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Analysis(Directory.GetCurrentDirectory() + @"\texts\t2_base64.bz2");
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Analysis(Directory.GetCurrentDirectory() + @"\texts\t3_base64.bz2");
+            //Console.ReadKey();
+            //
+        }
+
+        static void Analysis(string path)
+        {
+            Console.WriteLine("  " + path);
+            FileInfo fi = new FileInfo(path);
+
+            double total = 0;
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+            using (StreamReader sr = new StreamReader(path, Encoding.Default))
+            {
+                int i;
+                while ((i = sr.Read()) > -1)
+                {
+                    if (!dictionary.ContainsKey(Convert.ToChar(i)))
+                    {
+                        dictionary.Add(Convert.ToChar(i), 1);
+                    }
+                    else
+                    {
+                        dictionary[Convert.ToChar(i)] += 1;
+                    }
+                    total++;
+                }
+            }
+
+            double frequency = 0;
+            double entropy = 0;
+            double total_entropy = 0;
+            foreach (KeyValuePair<char, int> i in dictionary)
+            {
+                frequency = i.Value / total;
+                entropy = frequency * Math.Log(1 / frequency, 2);
+                total_entropy += entropy;
+                Console.WriteLine("\t{0} - частота: {1:f8}, ентропія: {2:f8}", i.Key, frequency, entropy);
+                entropy = 0;
+                frequency = 0;
+            }
+            Console.WriteLine("\tЗагальна ентропія: {0:f8}", total_entropy);
+            Console.WriteLine("\tКількість інформації: {0:f8} byte, розмір файлу: {1} byte", (total * total_entropy)/8, fi.Length);
+        }
+
+    }
+}
---
+++ Lab1/CsLab1Part2/CsLab1Part2/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsLab1Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Create_Base64File(Directory.GetCurrentDirectory() + @"\texts\t1.txt");
+            Console.WriteLine("t1.txt Base64 Encoding...");
+            Create_Base64File(Directory.GetCurrentDirectory() + @"\texts\t2.txt");
+            Console.WriteLine("t2.txt Base64 Encoding...");
+            Create_Base64File(Directory.GetCurrentDirectory() + @"\texts\t3.txt");
+            Console.WriteLine("t3.txt Base64 Encoding...");
+            Console.WriteLine("Done!");
+        }
+
+        public static void Create_Base64File(string path)
+        {
+            byte[] data = Encoding.Default.GetBytes(File.ReadAllText(path));
+            char[] value = Base64Encoding(data);
+            using (var wr = new StreamWriter(path.Remove(path.Length-4) + "_base64.txt"))
+            {
+                wr.WriteLine(value);
+            }
+        }
+
+        public static char[] Base64Encoding(byte[] data)
+        {
+            int length, length2;
+            int blockCount;
+            int paddingCount;
+
+            length = data.Length;
+
+            if ((length % 3) == 0)
+            {
+                paddingCount = 0;
+                blockCount = length / 3;
+            }
+            else
+            {
+                paddingCount = 3 - (length % 3);
+                blockCount = (length + paddingCount) / 3;
+            }
+
+            length2 = length + paddingCount;
+
+            byte[] source2;
+            source2 = new byte[length2];
+
+            for (int x = 0; x < length2; x++)
+            {
+                if (x < length)
+                {
+                    source2[x] = data[x];
+                }
+                else
+                {
+                    source2[x] = 0;
+                }
+            }
+
+            byte b1, b2, b3;
+            byte temp, temp1, temp2, temp3, temp4;
+            byte[] buffer = new byte[blockCount * 4];
+            char[] result = new char[blockCount * 4];
+
+            for (int x = 0; x < blockCount; x++)
+            {
+                b1 = source2[x * 3];
+                b2 = source2[x * 3 + 1];
+                b3 = source2[x * 3 + 2];
+
+                temp1 = (byte)((b1 & 252) >> 2);
+
+                temp = (byte)((b1 & 3) << 4);
+                temp2 = (byte)((b2 & 240) >> 4);
+                temp2 += temp;
+
+                temp = (byte)((b2 & 15) << 2);
+                temp3 = (byte)((b3 & 192) >> 6);
+                temp3 += temp;
+
+                temp4 = (byte)(b3 & 63);
+
+                buffer[x * 4] = temp1;
+                buffer[x * 4 + 1] = temp2;
+                buffer[x * 4 + 2] = temp3;
+                buffer[x * 4 + 3] = temp4;
+
+            }
+
+            for (int x = 0; x < blockCount * 4; x++)
+            {
+                result[x] = SixBitToChar(buffer[x]);
+            }
+
+            switch (paddingCount)
+            {
+                case 0:
+                    break;
+                case 1:
+                    result[blockCount * 4 - 1] = '=';
+                    break;
+                case 2:
+                    result[blockCount * 4 - 1] = '=';
+                    result[blockCount * 4 - 2] = '=';
+                    break;
+                default:
+                    break;
+            }
+
+            return result;
+        }
+
+        private static char SixBitToChar(byte b)
+        {
+            char[] lookupTable = new char[64] {
+            'A','B','C','D','E','F','G','H','I','J','K','L','M',
+            'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+            'a','b','c','d','e','f','g','h','i','j','k','l','m',
+            'n','o','p','q','r','s','t','u','v','w','x','y','z',
+            '0','1','2','3','4','5','6','7','8','9','+','/'};
+
+            if ((b >= 0) && (b <= 63))
+            {
+                return lookupTable[(int)b];
+            }
+            else
+            {
+                return ' ';
+            }
+        }
+    }
+}
